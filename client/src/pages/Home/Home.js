064c97d1a88f875f96ce06e21f91df9e0d9a3e71
brian nelson <bn3l10@gmail.com>
2022-11-12 20:32:13 -0500

    edit home

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,17 +1,26 @@
 import Sidebar from "../../components/SideBar/index";
 import Feed from "../../components/Feed/index";
 import ProfileBar from "../../components/ProfileBar/index"
+import { QUERY_POSTS } from "../../utils/queries";
+import { useQuery } from '@apollo/client';
 import "./home.css"
 
-export default function Home() {
+ const Home = () => {
+  const { data } = useQuery(QUERY_POSTS);
+  //const { data: userData } = useQuery(QUERY_ME);
+  const posts = data?.posts || [];
   return (
     <>
       
       <div className="homeContainer">
         <Sidebar />
-        <Feed/>
+        <Feed
+        posts={posts}
+        />
         <ProfileBar />
       </div>
     </>
   );
 }
+
+export default Home;
