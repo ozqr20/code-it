56d4ae4a230ab5e02215678d1df63c459886420f
brian nelson <bn3l10@gmail.com>
2022-11-16 20:35:21 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/code-it', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/codeit', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
